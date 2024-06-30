9e7d5054ed4eef8e53b4f2c4c88dfa23863c7b21
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-06-30 17:45:51 -0600

    Adding input to edit the to do

--- src/pages/TodoDetail.jsx
+++ src/pages/TodoDetail.jsx
@@ -7,12 +7,20 @@ const TodoDetail = ({ todos, setTodos }) => {
   const todo = todos.find((todo) => todo.id === parseInt(id));
 
   const [title, setTitle] = useState(todo ? todo.title : "");
+  const [isEditing, setIsEditing] = useState(false);
 
   const handleEdit = () => {
     const updatedTodos = todos.map((t) =>
       t.id === todo.id ? { ...t, title } : t
     );
     setTodos(updatedTodos);
+    setIsEditing(false);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleEdit();
+    }
   };
 
   return (
@@ -20,7 +28,22 @@ const TodoDetail = ({ todos, setTodos }) => {
       <BackButton />
       <div className="flex items-center justify-center h-full">
         <div className="container mx-auto p-1 pt-2 max-w-lg rounded-lg">
-          <h1 className="text-4xl text-center mb-28">{`TO DO ${todo.title}`}</h1>
+          {isEditing ? (
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              onKeyDown={handleKeyPress}
+              onBlur={handleEdit}
+              autoFocus
+              className="text-4xl text-center mb-28 border p-2 rounded-lg w-full"
+            />
+          ) : (
+            <h1
+              className="text-4xl text-center mb-28 cursor-pointer"
+              onClick={() => setIsEditing(true)}
+            >{`TO DO ${todo.title}`}</h1>
+          )}
           <h2 className="text-xl mb-2">{`Edit name of ${todo.title}`}</h2>
           <div className="flex items-center space-x-2 mb-20">
             <input
